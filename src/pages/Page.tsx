3205bfda20d4a4146786b8dd874d11587852ae86
Shubham Goel <goelsg@amazon.com>
2020-05-02 18:11:21 +0530

    Adding more libraries

--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -1,8 +1,11 @@
 import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { Component, NgZone } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation';
 import React from 'react';
 import { useParams } from 'react-router';
 import ExploreContainer from '../components/ExploreContainer';
 import './Page.css';
+import { construct } from 'ionicons/icons';
 
 const Page: React.FC = () => {
 
