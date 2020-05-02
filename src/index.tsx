a954105695465d011ae004c95c4b27c575bc3e1c
Shubham Goel <goelsg@amazon.com>
2020-05-02 19:55:10 +0530

    Gelocation Card

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+import { Geolocation } from '@ionic-native/geolocation';
+class MyApp extends Geolocation {
+    lattitude : number = 0 ;
+    longitude : number = 0 ;  
+    constructor() {
+      super();
+      this.watchPosition().subscribe(data => {
+        this.lattitude = data.coords.latitude;
+        this.longitude = data.coords.longitude;
+        console.log(this.longitude + "------" + this.lattitude);
+      })
+      console.log("Location Set");
+    };
+  }
+  var element = (<h1>Not working</h1>);
+  const variable = new MyApp;
 ReactDOM.render(<App />, document.getElementById('root'));
-
+variable.watchPosition().subscribe(data => {
+    element = (
+    <li>
+      <ul> lattitude : {data.coords.latitude}</ul>
+      <ul> longitude : {data.coords.longitude} </ul>
+    </li>
+  )
+  ReactDOM.render(element, document.getElementById('lister'));
+});
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -1,16 +1,9 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import { Component, NgZone } from '@angular/core';
-import { Geolocation } from '@ionic-native/geolocation';
+import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/react';
 import React from 'react';
 import { useParams } from 'react-router';
-import ExploreContainer from '../components/ExploreContainer';
 import './Page.css';
-import { construct } from 'ionicons/icons';
-
 const Page: React.FC = () => {
-
   const { name } = useParams<{ name: string; }>();
-
   return (
     <IonPage>
       <IonHeader>
@@ -27,10 +20,15 @@ const Page: React.FC = () => {
             <IonTitle size="large">{name}</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name={name} />
+        <IonCard>
+          <IonCardHeader> Geolocation </IonCardHeader>
+          <IonCardTitle> Current Location </IonCardTitle><br/>
+          <IonCardSubtitle> Getting CurrentPosition</IonCardSubtitle><br/>
+          <IonCardContent id = "lister">
+          </IonCardContent>
+          </IonCard>
       </IonContent>
     </IonPage>
   );
 };
-
 export default Page;
